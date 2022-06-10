6cac6e2229c4849d0d0f178b29c3d0d2a47ba0d6
ali_sarmadi <mr.snaros@gmail.com>
2022-06-10 19:47:51 +0430

    list test e2e

--- ui_product/cypress/e2e/list.cy.js
+++ ui_product/cypress/e2e/list.cy.js
@@ -1,8 +1,14 @@
+import { sampleData } from './../fixtures/mockData';
 describe('Navigation', () => {
-    it('should navigate to the about page', () => {
-      // Start from the index page
-      cy.visit('http://localhost:3000/list')
-      
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/list');
 
-    })
-  })
+    for (let i in sampleData) {
+      cy.get('[data-cy=listItem]')
+        .eq(i)
+        .should('contain', sampleData[i].name)
+        .should('contain', sampleData[i].username);
+    }
+  });
+});
